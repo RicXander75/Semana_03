c873ff0ee8204a6e7f11eae18f4b418f1de456f1
RicXander75 <78831216+RicXander75@users.noreply.github.com>
2021-02-09 21:12:04 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tarea2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x=0, suma = 0;
+            do
+            {
+                Console.WriteLine("Introduzca su numero: ");
+                x = int.Parse(Console.ReadLine());
+                suma = suma + x;
+            } 
+            while (x!=0);
+            {
+                Console.WriteLine("La suma de todos los numeros es: "+suma);
+            }
+
+        }
+    }
+} 
