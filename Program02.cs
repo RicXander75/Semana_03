0cc6b5db67a488efccb02622cdfa40335282e734
RicXander75 <78831216+RicXander75@users.noreply.github.com>
2021-02-09 21:16:02 -0400

    Add files via upload

---
+++ Program02.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace tarea2
+{
+    class Ejercicio_2
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 1;
+            do
+            {
+                Console.WriteLine(x);
+                x = x + 1;
+            }
+            while (x <= 10);
+            {
+                Console.WriteLine("Lista de los numeros del 1-10");
+
+
+            }
+        }
+
+    }
+}
+
---
+++ Program03.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 10 * 10;
+            do
+            {
+                Console.WriteLine(x);
+                x = x + 2;
+            }
+            while (x <= 120);
+            {
+                Console.WriteLine("Estos son los primeros 10 numeros pares a partir del producto de 10*10");
+            }
+        }
+    }
+}
